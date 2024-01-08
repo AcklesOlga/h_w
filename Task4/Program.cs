bd529e91acf19d79c8dae99a91b1bdf36a055d1c
AcklesOlga <154496742+AcklesOlga@users.noreply.github.com>
2024-01-08 23:41:35 +0400

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+int N = 15689;
+while (N > 10)
+{
+    int currentD = N % 10;
+    N = N / 10;
+
+    {
+        Console.Write(currentD + ",");
+    }
+}
+
+if (N < 10)
+{
+    Console.WriteLine(N);
+}
