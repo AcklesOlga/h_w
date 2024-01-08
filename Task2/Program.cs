6cd608d8012cd72078d3bfb7a4caf80598c949f2
AcklesOlga <154496742+AcklesOlga@users.noreply.github.com>
2024-01-08 23:39:11 +0400

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+int X = 5;
+int Y = -3;
+
+if (X > 0)
+{
+    if (Y > 0)
+    {
+        Console.WriteLine("1");
+    }
+    else
+    {
+        Console.WriteLine("4");
+    }
+}
+else
+{
+    if (Y > 0)
+    {
+        Console.WriteLine("2");
+    }
+    else
+    {
+        Console.WriteLine("3");
+    }
+}
