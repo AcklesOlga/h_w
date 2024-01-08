3b5bd56e6f620f3cb52d0815c3e4328167715ac5
AcklesOlga <154496742+AcklesOlga@users.noreply.github.com>
2024-01-08 23:40:04 +0400

    Add files via upload

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+int a = 64;
+if (a > 10 && a < 99)
+{
+    int firstD = a / 10;
+    int secondD = a % 10;
+    Console.WriteLine(Math.Max(firstD, secondD));
+}
+else
+{
+    Console.WriteLine ("Используйте число в отрезке [10; 99]");
+}
